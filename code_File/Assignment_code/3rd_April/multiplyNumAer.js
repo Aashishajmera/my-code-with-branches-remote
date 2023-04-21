2ca59dc41f3ab1d1cab09f04d3840a46c3a6a1ff
Aashishajmera <ajmeraaashu55@gmail.com>
2023-04-21 23:17:37 +0530

    assignment file code update

--- code_File/Assignment_code/3rd_April/multiplyNumAer.js
+++ code_File/Assignment_code/3rd_April/multiplyNumAer.js
@@ -1,6 +1,9 @@
-console.log(multiplyNumAer);
+// console.log(result);
+console.log(multiplyNumAer(3,6));
 
-var multiplyNumAer = function (num1 = 3, num2 = 5) {
+var multiplyNumAer = function (num1, num2) {
   let product = num1 * num2;
   return product;
 };
+
+// var result = multiplyNumAer(4, 6)
---
+++ code_File/Assignment_code/3rd_April/threeVariable.js
@@ -0,0 +1,9 @@
+{
+  console.log(var1);
+  console.log(var2);
+  console.log(var3);
+
+  let var1 = 4;
+  var var2 = 1;
+  const var3 = 3;
+}
