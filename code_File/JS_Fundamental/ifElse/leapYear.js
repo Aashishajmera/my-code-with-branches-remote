85de9835b4a399744ff12caeaf6297570e8dccf7
Aashishajmera <ajmeraaashu55@gmail.com>
2023-04-28 09:19:13 +0530

    update leap year file code

--- code_File/JS_Fundamental/ifElse/leapYear.js
+++ code_File/JS_Fundamental/ifElse/leapYear.js
@@ -1,7 +1,8 @@
 const prompt = require("prompt-sync")();
-const year = prompt("Enter a year: ");
-if(year % 4 == 0){
+const year = parseInt(prompt("Enter a year: "));
+if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
     console.log("given year ", year, "is leap year");
 } else {
     console.log("given year ", year , "is not leap year");
 }
+
